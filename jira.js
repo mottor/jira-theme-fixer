1f34c9a713f4ea3cefa69041ef52085feed4d96c
Aleksandr Bakulin <bakulin.a.v@yandex.ru>
2019-03-07 22:07:43 +0500

    added js and css

---
+++ jira.js
@@ -0,0 +1,29 @@
+// Скрипт для скролла доски с помощью мыши по образу Трелло
+// $('#ghx-pool').off('mousedown.trello-scroll');
+$(window).on('mousedown.trello-scroll', '#ghx-pool', function(e){
+    var $target = $(e.target);
+    var $closestParent = $target.closest('.ghx-issue, #ghx-pool');
+
+    if ($closestParent.hasClass('ghx-issue')){
+        return;
+    }
+
+    var $pool = $('#ghx-pool');
+    var startX = e.clientX;
+    var startY = e.clientY;
+    var startScrollX = $pool.scrollLeft();
+    var startScrollY = $pool.scrollTop();
+
+    $(window).on('mousemove.trello-scroll', function(moveEvent){
+        $('#ghx-pool')
+            .scrollLeft(startScrollX + (startX - moveEvent.clientX))
+            .scrollTop(startScrollY + (startY - moveEvent.clientY));
+    });
+
+    $(window).on('mouseup.trello-scroll', function(z){
+        $(window).off('mousemove.trello-scroll');
+        $(window).off('mouseup.trello-scroll');
+    });
+
+    e.preventDefault();
+});
