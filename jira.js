77f2a0385d3c6a73ca908c855b488e18873f6d2d
Aleksandr Bakulin <bakulin330@users.noreply.github.com>
2019-03-13 13:51:02 +0500

    hiding issue preview on Esc

--- jira.js
+++ jira.js
@@ -29,11 +29,13 @@ $(window).on('mousedown.trello-scroll', '#ghx-pool', function(e){
 });
 
 
-/* if ($('body').hasClass('ghx-scroll-columns')) {
+if ($('body').hasClass('ghx-scroll-columns')) {
     $(window).on('keydown.close-issue-preview', function(e) {
-        var $detailsWnd = $('.ghx-detail-view');
-        if ($detailsWnd.length && $detailsWnd.is(':visible')) {
-            $('.ghx-detail-view .aui-iconfont-close-dialog').click();
+        if (e.keyCode == 27) {
+            var $detailsWnd = $('.ghx-detail-view');
+            if ($detailsWnd.length && $detailsWnd.is(':visible')) {
+                $('.ghx-detail-view .aui-iconfont-close-dialog').click();
+            }
         }
     });
-} */
+}
